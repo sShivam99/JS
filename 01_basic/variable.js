6bfac21a448bdfde558c50809c8ccedf95ae0d83
sShivam99 <iamshivamsinha99@gmail.com>
2024-11-03 16:35:28 +0000

    variables

---
+++ 01_basic/variable.js
@@ -0,0 +1,21 @@
+const accountID =144
+let accountEmail="shivamsinha99@gmail.com"
+var accountPassword="shivamsinha99"
+accountCity="Varanasi"
+
+// accountID=2
+// variable initialised with const can't be re-declared. const remains constant throught the code 
+console.log(accountID);
+
+accountEmail='iamshivamsinha99@gmail.com'
+accountPassword="password"
+accountCity="Lucknow"
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountEmail,accountID,accountPassword,accountCity]);
+
+
+
