7f620697a0ae9692dda85c2f789b29ddd5bcfb4a
sShivam99 <iamshivamsinha99@gmail.com>
2024-12-10 22:01:25 +0000

    Spread,flat and from method

---
+++ array_01.js
@@ -0,0 +1,21 @@
+
+// Spread operator //
+// const marvel_heros=['Ironman','Captain','Thor'];
+// const dc_heros=['Superman',"Batman"];
+
+// const all_heroes=[...marvel_heros,...dc_heros]; 
+// console.log(all_heroes);
+
+// Flat operator //
+// const array=[1,2,3,[4,5],6,[7,[8,9]]];
+// const flat_array=array.flat(Infinity);
+
+// console.log(flat_array);
+
+// converting into array // imp !!
+
+const data=Array.from('Shivam');
+console.log(data);
+
+const objData=Array.from({name:'Shivam'});
+console.log(objData);
